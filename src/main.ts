a233567851a4d2dd5c182f544fa2cff4303525d2
Mohamed-Eid01 <mohamedaid8331@gmail.com>
2025-12-23 15:54:42 +0200

    last one

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as fs from 'fs';
-import * as path from 'path';
 
 async function bootstrap() {
-  // Create uploads directory if it doesn't exist
-  const uploadsDir = path.join(process.cwd(), 'uploads');
-  if (!fs.existsSync(uploadsDir)) {
-    fs.mkdirSync(uploadsDir, { recursive: true });
-  }
-
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://ecommerce-e-shop-withbackend.vercel.app', process.env.FRONTEND_URL],
+    origin: [
+      'http://localhost:5173',
+      'https://ecommerce-e-shop-withbackend.vercel.app',
+      process.env.FRONTEND_URL,
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
     credentials: true,
   });
+
   app.setGlobalPrefix('api');
-  
-  // Add ValidationPipe to enable DTO validation
+
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // Strip properties that don't have any validation decorators
-      forbidNonWhitelisted: true, // Throw errors if non-whitelisted properties are present
-      transform: true, // Automatically transform payloads to be objects typed according to their DTO classes
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
-  
-  await app.listen(process.env.PORT ?? 8008);
+
+  // 👇 مهم جدًا لـ Vercel
+  const server = app.getHttpAdapter().getInstance();
+  return server;
 }
-bootstrap();
+
+export default bootstrap();
